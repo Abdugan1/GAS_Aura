63cd68d08ddb66ef33674f7bccc9bee41b2a0f58
Abdugani <60340950+Abdugan1@users.noreply.github.com>
2025-04-06 10:33:11 +0500

    Custom Gameplay Effect. Beginning

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+public:
+	//** BOILERPLATE */
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		// Doesn't work FGameplayEffectContext::StaticStruct()... or I think so.
+		// Works fine with plain StaticStruct()
+		return StaticStruct(); 
+	}
+
+	virtual FGameplayEffectContext* Duplicate() const override;
+	
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess) override;
+	//** end BOILERPLATE */
+
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit() { bIsCriticalHit = true; }
+	void SetIsBlockedHit() { bIsBlockedHit = false; }
+	
+protected:
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+};
+
+/** BOILERPLATE */
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true,
+	};
+};
+/** end BOILERPLATE */
